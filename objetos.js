219561756cf4290bdaad4a55dae7c682744cf7da
coa <coa_777@hotmail.com>
2018-06-29 21:40:55 -0500

    Definiendo Objetos - Object.create

--- objetos.js
+++ objetos.js
@@ -1,24 +1,25 @@
-function Punto(x, y) {
-    this.x = x,
-    this.y = y
+const Punto = {
+    init: function(x, y){
+        this.x = x
+        this.y = y
+    },
+    moverEnX: function moverEnX(x) {
+        this.x += x
+    },
+    moverEnY: function moverEnY(y) {
+        this.y += y
+    },
+    distancia: function distancia(p) {
+        const x = this.x - p.x
+        const y = this.y - p.y
+        return (Math.sqrt(x*x + y*y)).toFixed(2)    
+    }
 }
 
-Punto.prototype.moverEnX = function moverEnX(x) {
-    this.x += x
-}
-
-Punto.prototype.moverEnY = function moverEnY(y) {
-    this.y += y
-}
-
-Punto.prototype.distancia = function distancia(p) {
-    const x = this.x - p.x
-    const y = this.y - p.y
-    return (Math.sqrt(x*x + y*y)).toFixed(2) 
-}
-
-const p1 = new Punto(0,4)
-const p2 = new Punto(3,0)
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
+p1.init(0,4)
+p2.init(3,0)
 
 console.log(p1.distancia(p2))
 p1.moverEnX(10)
